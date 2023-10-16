e9bd9bf2b9c328a5823d6c6ee0eb20309c5d4a67
Kalyg <122315970+M8317@users.noreply.github.com>
2023-10-16 08:39:27 +0200

    Home.js

--- Home.js
+++ Home.js
@@ -1,49 +1,61 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View, TouchableOpacity, ScrollView, Image, SafeAreaView } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import MenuItem from './MenuItem.js';
+import Panier from './Panier';
 
-export default function Home() {
+export default function Home({ navigation }) {
+  
+  const [cart, setCart] = useState([]);
+
+  const addToCart = (item) => {
+    setCart([...cart, item]);
+  };
+
+  const removeFromCart = (item) => {
+    setCart((prevCart) => prevCart.filter((cartItem) => cartItem !== item));
+  };  
+
   return (
-    <SafeAreaView style={{flex: 1}}>
-      <View style={styles.container}>
-        <View style={styles.header}>
-          <View style={styles.left}></View>
-          <Text style={styles.headerText}>Delivecrous</Text>
-          <TouchableOpacity style={styles.cartButton}>
-            <Ionicons name="cart" size={24} color="black" />
-          </TouchableOpacity>
-        </View>
-        <ScrollView>
-          <Text style={styles.title}>La carte</Text>
-          {menuItems.map((item, index) => (
-            <MenuItem key={index} item={item} />
-          ))}
-        </ScrollView>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.headerText}>Delivecrous</Text>
+        <TouchableOpacity 
+        style={styles.cartButton}
+        onPress={() => navigation.navigate('Panier', { panierItems: cart })}
+        >
+          <Ionicons name="cart" size={24} color="black" />
+        </TouchableOpacity>
       </View>
+      <ScrollView>
+        <Text style={styles.title}>La carte</Text>
+        {menuItems.map((item, index) => (
+          <MenuItem 
+          key={index} 
+          item={item} 
+          navigation={navigation} 
+          addToCart={addToCart} 
+          removeFromCart={removeFromCart} 
+          />
+        ))}
+      </ScrollView>
     </SafeAreaView>
   );
 }
-
-
-
-
-
 
 const menuItems = [
   {
     image: require('./assets/image1.png'),
     title: 'Menu 1',
     description: 'Description du menu 1',
+    allergens: 'Allergène 1, Allergène 2',
   },
   {
     image: require('./assets/image2.png'),
     title: 'Menu 2',
     description: 'Description du menu 2',
+    allergens: 'Allergène 3, Allergène 4',
   },
-  
-    
-  
 ];
 
 const styles = StyleSheet.create({
@@ -53,17 +65,18 @@ const styles = StyleSheet.create({
   },
   header: {
     flexDirection: 'row',
-    justifyContent: 'center',
+    justifyContent: 'space-between',
     alignItems: 'center',
     padding: 20,
     borderBottomWidth: 1,
     borderBottomColor: '#ddd',
-    backgroundColor: '#fff0f5',  // Couleur de fond du bandeau
+    backgroundColor: '#fff0f5',
   },
   title: {
     fontSize: 42,
     fontWeight: 'bold',
     marginTop: 10,
+    textAlign: 'center',
   },
   headerText: {
     fontSize: 32,
