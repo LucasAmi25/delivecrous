92effea2bf00547719ea7f3699e6aea6c804d069
lucas <104008928+LucasAmi25@users.noreply.github.com>
2023-10-17 08:09:44 +0200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Home from './Home';
+import MenuDetails from './MenuDetails';
+import Panier from './Panier';
+
+const Stack = createStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="MenuDetails" component={MenuDetails} />
+        <Stack.Screen name="Panier" component={Panier} /> 
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
